b9ed932734ef659685386303232d8f3a5e989832
Chris Aligent <45612883+chrisaligent@users.noreply.github.com>
2019-01-29 11:54:20 +1030

    Update PayPal Tax Provider to support Shipping Tax

--- Provider/TaxProvider.php
+++ Provider/TaxProvider.php
@@ -56,10 +56,21 @@ class TaxProvider
     public function getTax($entity)
     {
         try {
+            /**
+             * NOTE: The code below has been modified to resolve an issue where no
+             * tax was passed to PayPal, causing the total amount to be invalid
+             * eg $11 items (inc tax) + $100 shipping (ex tax) = $121 total (inc tax)
+             * If the Products already include Tax, we need to obtain the Shipping Tax
+             * and include that
+             *
+             * This code *may* be deprecated once the Tax improvements in Oro 3.1.0-LTS are merged
+             */
             if ($this->taxationSettingsProvider->isProductPricesIncludeTax()) {
-                return null;
+                // Product Prices already include Tax, so we only want the Shipping Tax
+                return $this->taxManager->loadTax($entity)->getShipping()->getTaxAmount();
             }
 
+            // Return the Total Tax (Products + Shipping)
             return $this->taxManager->loadTax($entity)->getTotal()->getTaxAmount();
         } catch (\Throwable $exception) {
             $this->logger->info(
